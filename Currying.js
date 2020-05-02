01bb8d8a95d4cbbde1df7a5038eaf5dd48fa25ba
Francis <francispham89@gmail.com>
2020-05-02 13:56:53 -0700

    Introduced Currying

---
+++ Currying.js
@@ -0,0 +1,37 @@
+// Normal Function:
+let dragon = (name, size, element) =>
+  name + ' is a ' +
+  size + ' dragon that breathes ' +
+  element + '!'
+
+console.log("Normal: ", dragon("fluffykins", "tiny", "lightning"));
+console.log("--------");
+console.log("--------");
+
+
+/* Currying Function: */
+let curryingDragon =
+  name =>
+    size =>
+      element =>
+        name + ' is a ' +
+        size + ' dragon that breathes ' +
+        element + '!';
+
+// Return Chain of Functions:
+console.log("Currying: ", curryingDragon("fluffykins")("tiny")("lightning"));
+console.log("---");
+
+// Missing Return from The Last Function:
+console.log(
+  "Currying & Missing Return From The Last Function: ", 
+  curryingDragon("fluffykins")("tiny")
+);
+console.log("---");
+
+//  Return from Separate Functions:
+let fluffykinsDragon = curryingDragon("fluffykins");
+let tinyDragon = fluffykinsDragon("tiny");
+
+console.log('Return from Separate Functions :',tinyDragon('lighting'));
+
