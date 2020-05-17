7021c9dfed25a5e1b69ec51787f47a6c0a5ee8b3
Francis <francispham89@gmail.com>
2020-05-17 13:32:15 -0700

    Linked List: Implemented insert Method using append, prepend, and traverseToIndex Methods

--- LinkedList/createLinkedLists.js
+++ LinkedList/createLinkedLists.js
@@ -37,16 +37,60 @@ class LinkedList {
     this.tail = newNode;  //  Setting new Tails everytime New Nodes Added
     this.length++;
     return this;
-  }
+  }               // O(1)
 
-    // Adding Values to The Beginning of the Linked List:
+  // Adding Values to The Beginning of the Linked List:
   prepend(value) {
     const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
     return this;
-  }
+  }               // O(1)
+
+
+  // Inserting Values to a specific Location inside Linked List:
+  // Step1: Convert Linked Lists into Arrays:
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    // while Loop is best used in this situation because we do not know how long is the List. 
+    while (currentNode !==null) { 
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  };
+  // Step2: Compose insert Method:
+  insert(index, value) {
+    // Check Params:
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    if (index === 0) {
+      return this.prepend(value);
+    }
+    // If index are other than these above:
+    const newNode = new Node(value);
+    //Define the Index of the Node before the inserted Location
+    const leader = this.traverseToIndex(index-1); //See the Last Method below:
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+  };               // O(n)
+
+  // Traverse Method:
+  traverseToIndex(index) {
+    // Check Params:
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) { // Traversing
+      currentNode = currentNode.next;
+      counter++;
+    };
+    return currentNode;
+  };
 };
 
 const myLinkedList = new LinkedList(10);
@@ -54,5 +98,12 @@ myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
 myLinkedList.append(17);
+console.log('Array: ', myLinkedList.printList());
 
-console.log(myLinkedList);
+myLinkedList.insert(2,170);
+console.log('Array After Insert 170: ', myLinkedList.printList());
+myLinkedList.insert(99,180);
+console.log('Array After Insert 180: ', myLinkedList.printList());
+myLinkedList.insert(0,190);
+console.log('Array After Insert 190: ', myLinkedList.printList());
+console.log('Linked List: ', myLinkedList);
