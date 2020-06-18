51f630430df85f076f27b5a546aed131d134bd8a
Francis <francispham89@gmail.com>
2020-06-18 13:27:26 -0700

    Algorithm in Recursion: Reverse String Question

--- Algorithms/Recursion.js
+++ Algorithms/Recursion.js
@@ -81,3 +81,36 @@ console.log(
   'FibonacciR: ', FibonacciRecursive,
   ' Vs FibonacciI: ', FibonacciIterative
 );
+
+/*
+Implement a function that reverses a string using iteration...and then recursion!
+*/
+
+function reverseString(str) {
+  let arrayStr = str.split("");
+  let reversedArray = [];
+  //We are using closure here so that we don't add the above variables to the global scope.
+  function addToArray(array) {
+    if (array.length > 0) {
+      reversedArray.push(array.pop());
+      addToArray(array);
+    }
+    return;
+  }
+  addToArray(arrayStr);
+  return reversedArray.join("");
+}
+
+const ReverseString = reverseString("yoyo master");
+
+function reverseStringRecursive(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+  }
+}
+
+const ReverseStringRecursive = reverseStringRecursive("yoyo master");
+
+console.log("RS: ", ReverseString, " Vs RSR: ", ReverseStringRecursive);
