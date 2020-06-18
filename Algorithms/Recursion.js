8bbb234dc0671d0b1c26b1098849d34a025dfe2b
Francis <francispham89@gmail.com>
2020-06-18 12:06:04 -0700

    Algorithm in Recursion: Fibonacci Question

--- Algorithms/Recursion.js
+++ Algorithms/Recursion.js
@@ -45,5 +45,39 @@ function findFactorialIterative(number) {
 
 let AnswerRecursive = findFactorialRecursive(5);
 let AnswerIterative = findFactorialIterative(5);
-console.log('Recursive: ', AnswerRecursive);
-console.log('Iterative; ', AnswerIterative);
+console.log(
+  'Recursive: ', AnswerRecursive,
+  ' Vs Iterative; ', AnswerIterative
+);
+
+
+/*
+Interview Question: 
+Give a Number N return the Index of the Fibonacci sequence, where the Sequence is:
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+The Pattern of the Sequence is that each Value is the Sum of the 2 Previous Values,
+that means that for N = 5 => 2 + 3
+*/
+
+function fibonacciIterative(n) {                        //  O(N)
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  };
+  return arr[n];
+};
+let FibonacciIterative = fibonacciIterative(8);         //  O(2^N)
+
+function fibonacciRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n-2);
+};
+
+let FibonacciRecursive = fibonacciRecursive(8);
+
+console.log(
+  'FibonacciR: ', FibonacciRecursive,
+  ' Vs FibonacciI: ', FibonacciIterative
+);
