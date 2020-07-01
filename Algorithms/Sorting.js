1346d9a881ea5d7f6ad35b3f46aec158f155931b
Francis <francispham89@gmail.com>
2020-06-30 18:51:03 -0700

    Sorting Algorithms: JS Array Sort Method vs Bubble Sort

---
+++ Algorithms/Sorting.js
@@ -0,0 +1,38 @@
+/* JS Methods */ 
+const letters = ['a', 'd', 'c', 'e', 'r', 'b'];
+const  basket = [2, 54, 66, 33, 1, 3, 55, 5, 6, 7];
+
+letters.sort();
+// Sort Method will translate Number to Unicode before Sorting:
+basket.sort();
+'33'.charCodeAt(0);
+// To Sort basket Array:
+basket.sort(function (a, b) {
+  return a - b;
+})
+// The Time & Space Complexity of the Sort Method cannot be guaranteed as it is implementation dependent.
+
+
+/* Bubble Sort: */
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if(array[j] > array[j+1]) {
+        // Swap Numbers
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j+1] = temp;
+      }
+    }
+  }
+}
+
+bubbleSort(numbers);
+console.log(numbers);
+
+
+/* Selection Sort: */
+
