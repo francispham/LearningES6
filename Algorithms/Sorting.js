caa9a2ca356fc374cb2995d030e1f8843fb539fb
Francis <francispham89@gmail.com>
2020-07-09 20:33:35 -0700

    Sorting Algorithms: Insertion Sort

--- Algorithms/Sorting.js
+++ Algorithms/Sorting.js
@@ -54,3 +54,26 @@ function selection(array) {
 }
 
 selection(numbers);
+
+
+/* Insertion Sort: */ 
+function insertionSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    if (array[i] < array[0]) {
+      // Move the Number to the First Position
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // Find where this Number should go:
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          // Move the number to the Right Spot
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+  }
+}
+
+insertionSort(numbers);
+console.log(numbers);
