418a5c8375e2ef6d7585e2e5cc9f33f2c5ca6335
Francis <francispham89@gmail.com>
2020-08-04 13:49:29 -0700

    Searching/Traversing Algorithms: Interview Questions

---
+++ Algorithms/SearchingInterview.js
@@ -0,0 +1,13 @@
+/*
+1. If you know a solution is not far form the Root of the Tree: BFS
+
+2. if the Tree is very Deep and Solution are Rare: BFS (DFS will take very long)
+
+3. If the Tree is very wide: DFS (BFS will need too much Memory)
+
+4. If Solutions are frequent but located Deep in the Tree: DFS
+
+5. Determining whether a Path exists between two Nodes: DFS
+
+6. Finding the Shortest Path: BFS
+*/ 
