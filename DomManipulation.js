73e6e8777bc1dbc5d10501e6667c5bdca78994dd
Francis Pham <francispham89@gmail.com>
2021-05-08 12:09:43 -0700

    DOM Manipulation Review

---
+++ DomManipulation.js
@@ -0,0 +1,34 @@
+const body = document.body;
+// body.append("Append", " a Div");
+// body.appendChild("appendChild");
+
+const div = document.createElement("div");
+// div.innerText = "innerText Div";
+// div.textContent = "textContent Div";
+div.innerHTML = "<strong>innerHTML</strong>"
+
+const strong = document.createElement("strong");
+strong.innerText = "innerText & strong"
+
+const divII = document.querySelector("div");
+
+// Calling:
+body.append(strong);
+body.append(div);
+console.log(divII.textContent);
+console.log(divII.innerText);
+
+
+// Attribute Manipulation:
+const spanId = document.querySelector("#Id")
+const spanTitle = document.querySelector("#title")
+console.log('Id:', spanId.getAttribute("id"));
+console.log('Title:', spanId.title);
+
+console.log('Modify Title:', spanId.setAttribute("title", "newTitle"));
+console.log('Modify Id:', spanId.id = 'newId');
+
+spanId.removeAttribute("title");
+
+
+// Data Attribute Manipulation:
