0b89d74648271571f399a5d03c8b31f9528f4408
Francis Pham <francis.pham.ca@gmail.com>
2025-04-15 16:15:50 -0700

    DOM Traversal

--- DomManipulation.js
+++ DomManipulation.js
@@ -12,13 +12,54 @@ strong.innerText = "innerText & strong";
 
 const divII = document.querySelector("div");
 
-// Calling:
+/* Adding DOM Elements */
 body.append(strong);
 body.append(div);
 console.log(divII.textContent);
 console.log(divII.innerText);
 
-// Attribute Manipulation:
+/* Downward DOM Traversal */
+const grandParent = document.getElementById("grandparent-id");
+
+function changeColor(element) {
+  element.style.backgroundColor = "#333";
+}
+
+// Change the background color of the grandparent element
+// changeColor(grandParent, "#333");
+
+// Use getElementsByClassName to get all elements with the class name "parent"
+// This returns an HTMLCollection, which is a live collection of elements
+const parents = document.getElementsByClassName("parent");
+
+// parents.forEach(changeColor); => This will not work as getElementsByClassName returns an HTMLCollection, not an array;
+
+// Using Array.from to convert HTMLCollection to an array
+const parentsArray = Array.from(parents);
+// parentsArray.forEach(changeColor);
+
+// Use querySelectorAll to get all elements with the class name "parent"
+const parentsQuery = document.querySelectorAll(".parent");
+// parentsQuery.forEach(changeColor); // => This will work as querySelectorAll returns a NodeList, which is iterable
+
+// Use querySelector to get the first element with the class name "parent"
+const firstParent = document.querySelector(".parent");
+const children = Array.from(firstParent.children);
+// children.forEach(changeColor);
+
+/* Upward DOM Traversal */
+const childOne = document.querySelector("#child-one");
+const parent = childOne.parentElement;
+const grandParentUpward = childOne.closest("#grandparent-id");
+
+/* DOM Siblings Traversal */
+// Get the next sibling of childOne
+const childTwo = childOne.nextElementSibling;
+
+// Change the background color of childOne
+changeColor(childTwo.previousElementSibling);
+
+/* Attribute Manipulation */
 const spanId = document.querySelector("#Id");
 const spanTitle = document.querySelector("#title");
 console.log("Id:", spanId.getAttribute("id"));
@@ -29,4 +70,4 @@ console.log("Modify Id:", (spanId.id = "newId"));
 
 spanId.removeAttribute("title");
 
-// Data Attribute Manipulation:
+/* Data Attribute Manipulation */
