23f33dd5541985f6c37733e2c9d00e2118e07a6e
Francis Pham <francis.pham.ca@gmail.com>
2025-04-16 09:52:11 -0700

    Add more Recursion notes

--- Algorithms/Recursion.js
+++ Algorithms/Recursion.js
@@ -1,25 +1,47 @@
-/* Rules: 
-1. Identify the Base Case ( A Conditional Statement )
-2. Identify the Recursive Case
-3. Get closer and closer and 'return' when needed
-=> Usually 2 'return' keywords are needed 
+/* RECURSION
+  + Recursion is a process in which a function calls itself as a subroutine.
+  + This means that the function will be called again and again until a base condition is met.
+  + Simply it's a function that refers to itself inside of that function.
+  + Anything that can be done with recursion can also be done with iteration.
+
+  + PROS:
+    1. Easy to understand
+    2. Easy to implement
+  + CONS:
+    1. Slow
+    2. Large call stack
+
+    + Tail Call Optimization: https://2ality.com/2015/06/tail-call-optimization.html
+
+    + How to write a Recursive Function: 
+      1. Identify the Base Case ( A Conditional Statement )
+      2. Identify the Recursive Case
+      3. Get closer and closer and 'return' when needed
+      => Usually 2 'return' keywords are needed!!!
+
+    + Rule:
+    Every time you are using a tree or converting something into a tree, consider RECURSION.
+      1. Divided into a number of subproblem that are similar to the original problem
+      2. Each instance of the subproblem is identical in nature.
+      3. The solutions of each subproblem can be combined to solve the original problem at hand.
+    => Divide and Conquer
 */
 
 let counter = 0;
 function inception() {
+  // Base Case that stops the recursion
   if (counter > 3) {
     return "done!";
   }
   counter++;
-  // inception(); => without return keyword, this function will return Undefined
+  // inception(); => without 'return' keyword, this function will return 'undefined'
   return inception();
 }
 // console.log("Inception:", inception());
 
-/*
-Interview Question:
-Write 2 Functions that finds the Factorial of any Number. One should use Recursive,
-the other should just use a for Loop.
+/* Interview Question:
+  Write 2 Functions that finds the Factorial of any Number. One should use Recursive,
+  the other should just use a for Loop.
 */
 function findFactorialRecursive(number) {
   if (number === 2) {
@@ -45,12 +67,11 @@ let AnswerRecursive = findFactorialRecursive(5);
 let AnswerIterative = findFactorialIterative(5);
 // console.log("Recursive: ", AnswerRecursive, " Vs Iterative: ", AnswerIterative);
 
-/*
-Interview Question: 
-Give a Number N return the Index of the Fibonacci sequence, where the Sequence is:
-0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
-The Pattern of the Sequence is that each Value is the Sum of the 2 Previous Values,
-that means that for N = 5 => 2 + 3
+/* Interview Question: 
+  Give a Number N return the Index of the Fibonacci sequence, where the Sequence is:
+  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+  The Pattern of the Sequence is that each Value is the Sum of the 2 Previous Values,
+  that means that for N = 5 => 2 + 3
 */
 
 function fibonacciIterative(n) {
@@ -78,9 +99,7 @@ let FibonacciRecursive = fibonacciRecursive(8);
 //   FibonacciIterative
 // );
 
-/*
-Implement a function that reverses a string using iteration...and then recursion!
-*/
+//? => Implement a function that reverses a string using iteration...and then recursion!
 
 function reverseString(str) {
   let arrayStr = str.split("");
